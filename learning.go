6f940491c7f8a1ab3fe5ccf2b6b762294113da80
shai schnarch <shai.schnarch@gmail.com>
2025-02-17 16:13:56 +0200

    added simple godoc

--- learning.go
+++ learning.go
@@ -1,5 +1,9 @@
+// Package Learning Go
+// Simple packege with add function for practicing writing a public go library
 package learninggo
 
+// Add func for adding two integers
+// https://www.mathisfun.com/numbers/addition.html
 func Add(x int, y int) int {
 	return x + y
 }
